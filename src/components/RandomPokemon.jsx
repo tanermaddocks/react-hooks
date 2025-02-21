9ede09971003b72049a46f2aaae2376f3f36f883
tanermaddocks <maddockstaner@gmail.com>
2025-02-22 09:23:45 +1000

    feat: hook for randomPokemon

--- src/components/RandomPokemon.jsx
+++ src/components/RandomPokemon.jsx
@@ -7,6 +7,25 @@ export function RandomPokemon() {
     // let [readOnlyData, functionToUpdateData] = useState("");
     // let [pokeData, setPokeData] = useState({name: "", sprite: ""});
 
+	async function getRandomPokemon(){
+		console.log("RandomPokemonComponent has loaded.");
+
+		let targetPokemonID = Math.floor(Math.random() * 1025) + 1;
+		console.log("Getting data for Pokemon with ID of : " + targetPokemonID);
+
+		let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + targetPokemonID);
+		let data = await response.json();
+
+        setName(data.name);
+        setSprite(data.sprites.front_default);
+
+        // return data;
+
+		// this.setState({ // Only for class components
+		// 	name: data.name,
+		// 	sprite: data.sprites.front_default
+		// });
+	};
     // useEffect(() => {
 
 	// }, []);
@@ -15,9 +34,13 @@ export function RandomPokemon() {
 	useEffect(() => {
 		console.log("Component has loaded!");
 
-		return (() => {
-			console.log("useEffect replicating onComponentDidMount is unloading.");
-		});
+        getRandomPokemon()
+
+        // getRandomPokemon().then(data => {
+		// 	setName(data.name);
+		// 	setSprite(data.sprites.front_default);
+		// });
+
 	}, []);
 
 	// componentDidUpdate
