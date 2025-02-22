0707ec7040752913e32252929bf97601f9dd3064
tanermaddocks <maddockstaner@gmail.com>
2025-02-22 12:07:02 +1000

    asad

--- src/contexts/UserJwtContext.jsx
+++ src/contexts/UserJwtContext.jsx
@@ -1,10 +1,20 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { defaultUserJwtData, UserJwtContext } from "../hooks/useUserJwtData";
+import { useLocalStorage } from "react-use";
 
 export function UserJwtProvider({children}){
 	let [userJwtData, setUserJwtData] = useState(defaultUserJwtData);
+	let [jwtsPersisted, setJwtsPersisted] = useLocalStorage('jwts', defaultUserJwtData);
+
+	useEffect(() => {
+		setJwtsPersisted(userJwtData);
+	}, [setJwtsPersisted, userJwtData]);
+
+	useEffect(() => {
+		setUserJwtData(jwtsPersisted);
+	}, []);
 
 	return(
 		<UserJwtContext.Provider value={[userJwtData, setUserJwtData]}>
