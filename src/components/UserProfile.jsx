f14bb0fae40de4b9c3f59dd8294de59feff2a9e7
tanermaddocks <maddockstaner@gmail.com>
2025-02-22 11:30:09 +1000

    Another commit

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import { UserJwtContext } from "../contexts/UserJwtContext";
-
+import { jwtDecode } from "jwt-decode";
 
 export function UserProfile(){
 	let [userJwtData] = useContext(UserJwtContext);
+
 
 	if (userJwtData.accessToken){
 		return (
 			<div>
 				<h2>{userJwtData.accessToken}</h2>
+				<h2>{jwtDecode(userJwtData.accessToken).sub}</h2>
 			</div>
 		)
 	} else {
